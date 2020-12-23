5e8625de33902840f904d224130aace59d9d26de
Paula Micheli <paula.michele.581@gmail.com>
2020-12-23 10:26:26 -0300

    Theme Fonts

--- Flutter #1/act01/lib/main.dart
+++ Flutter #1/act01/lib/main.dart
@@ -9,9 +9,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.purple, // cor primaria que irá colori os elementos da
-        // aplicação. Aqui é um range de cores 
+        primarySwatch: Colors.purple, // cor primaria que irá colori os elementos da aplicação. Aqui é um range de cores 
         accentColor: Colors.amber,  // ira realcçar alguns elementos especificos da aplicação
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          // aqui eu estou acessando as propriedades do themelight e fazendo uma copia destas.
+          headline6: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+            headline6: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          )
+        )
       ),
       home: Dashboard(),
     );
